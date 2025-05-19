f32183ae055863a563a7169cbb702f0629b972f5
hkim628 <hongphuong89556@gmail.com>
2025-05-19 07:58:11 +0700

    Add files via upload

---
+++ js/ct.js
@@ -0,0 +1,9 @@
+ $(document).ready(function() {
+     $('.slideshow-congtrinh').slick({
+         autoplay: true,
+         autoplaySpeed: 3000,
+         dots: false,
+         arrows: false,
+         infinite: true,
+     });
+ });
